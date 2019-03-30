3b679d6abcd400b5768be536ca37258a74ffc867
jxmiller5 <jxmiller5@gmail.com>
2019-03-29 18:01:48 -0700

    added firebase initail code

---
+++ firebasejs.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyDbZLvUgQlFQjfrop7h_fUC0Z-97CKM5UY",
+    authDomain: "game-tinder.firebaseapp.com",
+    databaseURL: "https://game-tinder.firebaseio.com",
+    projectId: "game-tinder",
+    storageBucket: "",
+    messagingSenderId: "812307683576"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
