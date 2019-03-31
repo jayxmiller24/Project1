24789d91cf749aef1ab59c6b4d26c718bc455725
jxmiller5 <jxmiller5@gmail.com>
2019-03-31 01:41:23 -0700

    added search html and added javescript for search

--- assets/javascript/databaseapi.js
+++ assets/javascript/databaseapi.js
@@ -1,18 +1,29 @@
 $(document).ready(function () {
 
-  var gameUrl = "https://www.giantbomb.com/api/search/";
-  var apiKey = "2c81a4645598e2c114827c1fef8965cdd7309c04";
-  var apiKeyParameter = "?api_key="
-  var searchParameters = "&query="
-  var game = "zelda"
-  var gameParameters = "&format=JSON"
-  var resources = "&resources=game"
-  var queryUrl = gameUrl + apiKeyParameter + apiKey + gameParameters + searchParameters + game + resources;
-
   
+  //var gameUrl = "https://www.giantbomb.com/api/search/";
+  //var apiKey = "2c81a4645598e2c114827c1fef8965cdd7309c04";
+  //var apiKeyParameter = "?api_key="
+  //var searchParameters = "&query="
+  //var game = ""
+  //var gameParameters = "&format=JSON"
+  //var resources = "&resources=game"
+  //var queryUrl = gameUrl + apiKeyParameter + apiKey + gameParameters + searchParameters + game + resources;
+  
+  
+  
+  
+  $("#searchbutton").on("click", function (event) {
+    event.preventDefault();
+    
+    
+    var searchTerm = $("#search").val()
+    //console.log(searchTerm);
+    var queryURL = "https://www.giantbomb.com/api/search/?api_key=2c81a4645598e2c114827c1fef8965cdd7309c04&format=JSON&query=" + searchTerm + "&resources=game"
+    //console.log(queryURL);
     
     $.ajax({
-      url: queryUrl,
+      url: queryURL,
       method: "GET",
       dataType: "jsonp",
       jsonp: "json_callback",
@@ -21,10 +32,15 @@ $(document).ready(function () {
       }
     }).then(function (response) {
       //console.log(response.results);
-      var gameResults = response.results;
-      console.log(gameResults);
+      var gameInfo = response.results;
+      console.log(gameInfo);
       
-      //}
+    
+
     });
-  
+
+
+
+  });
+
 });
