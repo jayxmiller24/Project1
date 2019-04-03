fd1ab10f42d33330bdb029c811aad83a78102220
jxmiller5 <jxmiller5@gmail.com>
2019-04-02 20:23:22 -0700

    added a function to databaseapi

--- assets/javascript/databaseapi.js
+++ assets/javascript/databaseapi.js
@@ -10,15 +10,32 @@ $(document).ready(function () {
   //var resources = "&resources=game"
   //var queryUrl = gameUrl + apiKeyParameter + apiKey + gameParameters + searchParameters + game + resources;
 
+  $("#search").keypress(function (event) {
+    
+    
+    
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    
+    
+    if (keycode === 13) {
+      var searchTerm = $("#search").val()
+      displayInfo(searchTerm);
 
-
-
+    }
+  });
   $("#searchbutton").on("click", function (event) {
     event.preventDefault();
 
-
     var searchTerm = $("#search").val()
     //console.log(searchTerm);
+    displayInfo(searchTerm);
+
+
+
+  });
+  
+
+  function displayInfo(searchTerm) {
     var queryURL = "https://www.giantbomb.com/api/search/?api_key=2c81a4645598e2c114827c1fef8965cdd7309c04&format=JSON&query=" + searchTerm + "&resources=game"
     //console.log(queryURL);
 
@@ -36,15 +53,15 @@ $(document).ready(function () {
       //console.log(results)
       $("#appear-here").empty();
       for (var i = 0; i < results.length; i++) {
-        
-        
+
+
         var gameDiv = $("<div class='col-sm-3'>");
 
 
         var p = $("<p>").text("Description: " + results[i].deck);
-        
 
         var gameImage = $("<img>");
+
         gameImage.attr("src", results[i].image.icon_url);
         gameDiv.prepend(p);
         gameDiv.prepend(gameImage);
@@ -55,10 +72,10 @@ $(document).ready(function () {
 
 
 
+
+
     });
-
-
-
-  });
-
+    
+  }
+  displayInfo();
 });
