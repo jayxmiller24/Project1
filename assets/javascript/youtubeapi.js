88b5c5175ed69843bb1276b7e01e15c6d7df8423
Kyle Woodruff <kylewoodruff@gmail.com>
2019-04-06 13:37:18 -0700

    Displaying youtube results

--- assets/javascript/youtubeapi.js
+++ assets/javascript/youtubeapi.js
@@ -1,25 +1,23 @@
+function searchQuery(searchTerm) {
+  console.log('serarchTer',searchTerm)
 var ytQueryURL = "https://www.googleapis.com/youtube/v3/search";
 var ytAPIKey = "AIzaSyBscjeiTtWmqjgcV3Xoz67fgf-sW-hDPIY";
-var searchTerm = "Call of Duty";
+var clientId = "AIzaSyBscjeiTtWmqjgcV3Xoz67fgf-sW-hDPIY";
+var queryURL = "https://www.googleapis.com/youtube/v3/search";
+var options = {
+  part: "snippet",
+  key: clientId,
+  maxResults: 5,
+  q: searchTerm,
+  // videoCategoryId: "Gaming",
+  //videos are sorted from highest to lowest rating.
+  // order: "title",
+  regionCode: "US",
+  relevanceLanguage: "en",
+  topicID: "/m/0bzvm2",
+  type: "video"
+};
 
-$(document).ready(function() {
-  var clientId = "AIzaSyBscjeiTtWmqjgcV3Xoz67fgf-sW-hDPIY";
-  var queryURL = "https://www.googleapis.com/youtube/v3/search";
-  var options = {
-    part: "snippet",
-    key: clientId,
-    maxResults: 5,
-    q: searchTerm,
-    // videoCategoryId: "Gaming",
-    //videos are sorted from highest to lowest rating.
-    order:"rating"
-  };
+return {queryURL, options};
 
-  function getVids() {
-    $.getJSON(queryURL, options, function(data) {
-      console.log(data.items);
-    });
-  }
-  getVids();
-
-});
+};
