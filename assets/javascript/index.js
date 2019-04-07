5fa0ae550300ed3398b3345425e9c15e934e74fd
Kyle Woodruff <kylewoodruff@gmail.com>
2019-04-07 12:48:34 -0700

    clear game modal on hide

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -2,62 +2,76 @@ var auth = firebase.auth();
 var database = firebase.database();
 var userLogged = true;
 var uid = "";
-function setUid(id){
-uid = id;
+function setUid(id) {
+  uid = id;
 
-console.log(uid);
+  console.log(uid);
 }
 
-$(document).ready(function () {
-    $('.modal').modal();
+$(document).ready(function() {
+  // $(".modal").modal({
+  //   onCloseEnd: onModalHide
+  //  });
+  $("#modal-login").modal({});
+  $("#modal-signup").modal({});
+  $("#game").modal({
+    onCloseEnd: onGameModalHide
+   });
 });
 
-$(document).on("click", ".card-panel", function(){
-    console.log("clicked image");
-    var cardImage= $(this).find("img");
-    var mainTitle = $(this).find("h6").text();
-    var mainP= $(this).find("p").text();
-    // console.log(cardImage);
-    // console.log(mainTitle);
-    // console.log(mainP);
+$(document).on("click", ".card-panel", function() {
+  console.log("clicked image");
+  var cardImage = $(this).find("img");
+  var mainTitle = $(this)
+    .find("h6")
+    .text();
+  var mainP = $(this)
+    .find("p")
+    .text();
+  // console.log(cardImage);
+  // console.log(mainTitle);
+  // console.log(mainP);
 
-    var imgSource = $(cardImage).attr("src");
-    // console.log(imgSource);
-   $("#game img").attr("src", imgSource);
-   $("#game h4").text(mainTitle);
-   $("#game p").text(mainP);
-    
+  var imgSource = $(cardImage).attr("src");
+  // console.log(imgSource);
+  $("#game img").attr("src", imgSource);
+  $("#game h4").text(mainTitle);
+  $("#game p").text(mainP);
 
-    //console.log(mainTitle);
-   
-   const searchOBJ = searchQuery(mainTitle);
-   $.getJSON(searchOBJ.queryURL, searchOBJ.options, function(data) {
-    const results = data.items
+  //console.log(mainTitle);
+
+  const searchOBJ = searchQuery(mainTitle);
+  $.getJSON(searchOBJ.queryURL, searchOBJ.options, function(data) {
+    const results = data.items;
     console.log(results);
     results.forEach(i => {
-        let vidID = i.id.videoId;
-        console.log(vidID);
+      let vidID = i.id.videoId;
+      console.log(vidID);
 
-        let vidTitle = i.snippet.title;
-        console.log(vidTitle);
+      let vidTitle = i.snippet.title;
+      console.log(vidTitle);
 
-        let vidImg = i.snippet.thumbnails.default.url;
-        console.log(vidImg);
-        
-        let modalDiv = $("#youtube-list");
-        let newDiv = $("<div>")
-        let newheader = $("<h6>");
-        let newImage = $("<img>");
-        newDiv.attr("id", vidID);
-        newImage.attr("src", vidImg);
-        newDiv.append(newImage);
-        newheader.text(vidTitle);
-        newDiv.append(newheader);
-        modalDiv.append(newDiv);
+      let vidImg = i.snippet.thumbnails.default.url;
+      console.log(vidImg);
+
+      let modalDiv = $("#youtube-list");
+      let newDiv = $("<div>");
+      let newheader = $("<h6>");
+      let newImage = $("<img>");
+      newDiv.attr("id", vidID);
+      newImage.attr("src", vidImg);
+      newDiv.append(newImage);
+      newheader.text(vidTitle);
+      newDiv.append(newheader);
+      modalDiv.append(newDiv);
     });
   });
-    // $("#game").show();
-  });
+});
 
-
-
+// function to clear game modal when switching games
+var onGameModalHide = function() {
+  console.log("Modal closed!");
+  $("#game-image").attr("src", "" );
+  $("#game-description").empty();
+  $("#youtube-list").empty();
+};
